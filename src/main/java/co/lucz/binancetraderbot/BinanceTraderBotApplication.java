69a62ceddc96bd9dd0cdf17493193f3dc98d2dc5
Soma Lucz <luczsoma@gmail.com>
2021-12-28 16:45:46 +0100

    Initial commit

---
+++ src/main/java/co/lucz/binancetraderbot/BinanceTraderBotApplication.java
@@ -0,0 +1,13 @@
+package co.lucz.binancetraderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceTraderBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceTraderBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/lucz/binancetraderbot/BinanceTraderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package co.lucz.binancetraderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceTraderBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
